6a439be3ef86210fd9c7e5fb63ba1b96c8d6ef5d
Henrik Braune <hb@braune-digital.com>
2015-06-16 15:58:38 +0200

    Refactoring

---
+++ Request/ParamConverter/CityParamConverter.php
@@ -0,0 +1,74 @@
+<?php
+namespace BrauneDigital\GeoBundle\Request\ParamConverter;
+
+use Doctrine\ORM\EntityManager;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Sensio\Bundle\FrameworkExtraBundle\Request\ParamConverter\ParamConverterInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ConfigurationInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+class CityParamConverter implements ParamConverterInterface {
+
+
+	public function __construct(EntityManager $entityManager)
+	{
+		$this->em = $entityManager;
+	}
+
+	public function apply(Request $request, ParamConverter $configuration)
+	{
+
+		if ($request->attributes->get('city_id')) {
+			$city = $this->em->getRepository('BrauneDigitalGeoBundle:City')->find($request->attributes->get('city_id'));
+		} else {
+
+			$slug = $request->attributes->get('city');
+
+			$query = $this->em
+				->createQuery(
+					'SELECT c FROM BrauneDigitalGeoBundle:City c '
+					. 'JOIN c.translations ctr '
+					. 'WHERE ctr.slug = :slug OR c.id = :id '
+					. 'ORDER BY c.id asc ')
+				->setParameters(array(
+					'slug' => $slug,
+					'id' => $slug
+				));
+			$cities = $query->getResult();
+
+			// Care for old slugs
+			if (count($cities) == 0) {
+				$query = $this->em
+					->createQuery(
+						'SELECT c FROM BrauneDigitalGeoBundle:City c '
+						. 'WHERE c.oldSlug = :slug '
+						. 'ORDER BY c.id asc ')
+					->setParameters(array(
+						'slug' => $slug
+					));
+				$cities = $query->getResult();
+				if (count($cities) > 0) {
+					$city = $cities[0];
+					$request->attributes->set('redirect', true);
+				} else {
+					throw new NotFoundHttpException();
+				}
+			} else {
+				$city = $cities[0];
+			}
+		}
+
+		$param = $configuration->getName();
+		$request->attributes->set($param, $city);
+
+		return true;
+	}
+
+	public function supports(ParamConverter $configuration)
+	{
+		return "BrauneDigital\GeoBundle\Entity\City" === $configuration->getClass();
+	}
+
+
+}
---
+++ Request/ParamConverter/CountryParamConverter.php
@@ -0,0 +1,60 @@
+<?php
+namespace BrauneDigital\GeoBundle\Request\ParamConverter;
+
+use Doctrine\ORM\EntityManager;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Sensio\Bundle\FrameworkExtraBundle\Request\ParamConverter\ParamConverterInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ConfigurationInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+class CountryParamConverter implements ParamConverterInterface {
+
+	private $entityManager;
+
+	public function __construct(EntityManager $entityManager)
+	{
+		$this->em = $entityManager;
+	}
+
+    public function apply(Request $request, ParamConverter $configuration)
+    {
+
+        if ($request->attributes->get('country_id')) {
+            $city = $this->em->getRepository('BrauneDigitalGeoBundle:Country')->find($request->attributes->get('country_id'));
+        } else {
+
+            $slug = $request->attributes->get('country');
+            $query = $this->em
+                ->createQuery(
+                    'SELECT c FROM BrauneDigitalGeoBundle:Country c '
+                    . 'JOIN c.translations ctr '
+                    . 'WHERE ctr.slug = :slug OR c.id = :id '
+                    . 'ORDER BY c.id asc ')
+                ->setParameters(array(
+                    'slug' => $slug,
+                    'id' => $slug
+                ));
+            $countries = $query->getResult();
+
+            // Care for old slugs
+            if (count($countries) > 0) {
+                $country = $countries[0];
+            } else {
+                $country = null;
+            }
+        }
+
+        $param = $configuration->getName();
+        $request->attributes->set($param, $country);
+
+        return true;
+    }
+
+	public function supports(ParamConverter $configuration)
+	{
+		return "BrauneDigital\GeoBundle\Entity\Country" === $configuration->getClass();
+	}
+
+
+}
