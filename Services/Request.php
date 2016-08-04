5c71f163051de8f034796d835da5cac3aee62516
Patrick Rathje <pr@braune-digital.com>
2016-08-04 12:00:49 +0200

    Refactored Locales added StateRequest

--- Services/Request.php
+++ Services/Request.php
@@ -58,15 +58,84 @@ class Request {
             $response = $client->send($request);
             $result = $response->json();
 
-            if ($result != null && array_key_exists('adminId1', $result) && array_key_exists('geonameId', $result)) {
-
+            if ($result != null && array_key_exists('countryCode', $result)) {
                 $country = $this->em->getRepository('ApplicationBrauneDigitalGeoBundle:Country')->findOneByCode($result['countryCode']);
             }
         }
 
-        $entity->setCountry($country);
+        if ($country) {
+            $entity->setCountry($country);
+        }
+
         return $country;
     }
+
+
+    /**
+     * @param integer $geonameId
+     * @return State
+     */
+    public function requestState($geonameId, GeoInterface $entity)
+    {
+        $state = $this->container->get('doctrine')->getRepository('Application\BrauneDigital\GeoBundle\Entity\State')->findOneBy(array(
+            'geonameIdentifier' => $geonameId
+        ));
+
+        if (!$state) {
+            $state = new State();
+            $client = new Client();
+            $request = $client->createRequest('GET', 'http://api.geonames.org/getJSON?geonameId=' . $geonameId . '&username=' . $this->container->getParameter('geonames_user'));
+            $response = $client->send($request);
+            $result = $response->json();
+
+            if($result != null && array_key_exists('geonameId', $result)) {
+                $country = $this->em->getRepository('ApplicationBrauneDigitalGeoBundle:Country')->findOneByCode($result['countryCode']);
+                if ($country) {
+                    $state->setCountry($country);
+                }
+
+                $state->setLatitude($result['lat']);
+                $state->setLongitude($result['lng']);
+
+                $timezone = $this->em->getRepository('ApplicationBrauneDigitalGeoBundle:Timezone')->findOneByCode($result['timezone']['timeZoneId']);
+                if ($timezone) {
+                    $state->setTimezone($timezone);
+                }
+                $state->setGeonameIdentifier($result['geonameId']);
+                $state->setNameAscii($result['asciiName']);
+
+                $locales = $this->em->getRepository('BrauneDigitalTranslationBaseBundle:Language')->getEnabledCodes();
+
+                if (isset($result['name'])) {
+                    $state->translate('en')->setNameUtf8($result['name']);
+                }
+
+                foreach($result['alternateNames'] as $alternateName) {
+                    if (isset($alternateName['lang']) && in_array($alternateName['lang'], $locales)) {
+                        if(!$state->translate($alternateName['lang'])->getNameUtf8() || isset($alternateName['isPreferredName'])) {
+                            $state->translate($alternateName['lang'])->setNameUtf8($alternateName['name']);
+                        }
+                    }
+                }
+
+                $this->em->persist($state);
+                $state->mergeNewTranslations();
+                $this->em->flush();
+
+            } else {
+                $state = null;
+            }
+
+        }
+
+        if ($state != null) {
+            $entity->setState($state);
+        }
+
+        return $state;
+    }
+
+
 
     /**
      * @param integer $geonameId
@@ -88,26 +157,38 @@ class Request {
 
             if($result != null && array_key_exists('adminId1', $result) && array_key_exists('geonameId', $result)) {
                 $country = $this->em->getRepository('ApplicationBrauneDigitalGeoBundle:Country')->findOneByCode($result['countryCode']);
-                $city->setCountry($country);
+
+                if ($country) {
+                    $city->setCountry($country);
+                }
+
+                $state = $this->em->getRepository('ApplicationBrauneDigitalGeoBundle:Country')->findOneByCode($result['adminId1']);
+
+                if ($state) {
+                    $city->setState($state);
+                }
 
                 $city->setLatitude($result['lat']);
                 $city->setLongitude($result['lng']);
                 $city->setFcode($result['fcode']);
 
-                $city->setTimezone($this->em->getRepository('ApplicationBrauneDigitalGeoBundle:Timezone')->findOneByCode($result['timezone']['timeZoneId']));
+                $timezone = $this->em->getRepository('ApplicationBrauneDigitalGeoBundle:Timezone')->findOneByCode($result['timezone']['timeZoneId']);
+                if ($timezone) {
+                    $city->setTimezone($timezone);
+                }
                 $city->setGeonameIdentifier($result['geonameId']);
 
                 $locales = $this->em->getRepository('BrauneDigitalTranslationBaseBundle:Language')->getEnabledCodes();
+                
+                if (isset($result['name'])) {
+                    $city->translate('en')->setNameUtf8($result['name']);
+                }
 
                 foreach($result['alternateNames'] as $alternateName) {
-
-                    if (count($locales) == 0) {
-                        break;
-                    }
-
                     if (isset($alternateName['lang']) && in_array($alternateName['lang'], $locales)) {
-                        $city->translate($alternateName['lang'])->setNameUtf8($alternateName['name']);
-                        unset ($locales[$alternateName['lang']]);
+                        if(!$city->translate($alternateName['lang'])->getNameUtf8() || isset($alternateName['isPreferredName'])) {
+                            $city->translate($alternateName['lang'])->setNameUtf8($alternateName['name']);
+                        }
                     }
                 }
                 
@@ -121,7 +202,9 @@ class Request {
 
         }
 
-        $entity->setCity($city);
+        if ($city != null) {
+            $entity->setCity($city);
+        }
 
         return $city;
     }
